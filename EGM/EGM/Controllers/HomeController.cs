fb2eae59f755e43023738e38f95cf761ef3aebff
princejoseph1978 <princej1978@gmail.com>
2017-05-23 23:44:08 +1200

    egm
    EGM

---
+++ EGM/EGM/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using EGM.GameMachines.Core.DataAccess;
+using EGM.GameMachines.Core.Repository;
+using System.Web.Mvc;
+
+namespace EGM.Controllers
+{
+    public class HomeController : Controller
+    {        
+        private readonly IGameMachineRepository _gamerepo;
+
+        public HomeController(IGameMachineRepository gamerepo)
+        {
+            _gamerepo = gamerepo;
+        }
+        // GET Gamemachine
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET All Game machines
+        public JsonResult GetAllGameMachines()
+        {
+            var gamemachineList = _gamerepo.GetAllGameMachines();
+            return Json(gamemachineList, JsonRequestBehavior.AllowGet);            
+        }
+        //GET Game machine by Id
+        public JsonResult GetGamemachineById(string id)
+        {            
+            var getgamemachineById = _gamerepo.GetGamemachineById(id);
+            return Json(getgamemachineById, JsonRequestBehavior.AllowGet);            
+        }
+        //Update Game Machine
+        public string UpdateGamemachine(GameMachine gamemachine)
+        {
+            if (_gamerepo.UpdateGamemachine(gamemachine))
+                return "Game machine details updated successfully";
+            else
+                return "Invalid Game machine details.";
+            
+        }
+        // Add Game machine
+        public string AddGamemachine(GameMachine gamemachine)
+        {
+            if (_gamerepo.AddGamemachine(gamemachine))
+                return "Game machine details added successfully";
+            else
+                return "Invalid Game machine details.";
+            
+        }
+        // Delete Gamemachine
+        public string DeleteGamemachine(string gamemachineId)
+        {
+            if (_gamerepo.DeleteGamemachine(gamemachineId))
+                return "Selected Game machine deleted sucessfully";
+            else
+                return "Invalid operation";
+            
+        }
+    }
+}
---
+++ EGM/EGM/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using EGM.GameMachines.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace EGM
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            RegisterComponents();
+        }
+
+        private void RegisterComponents()
+        {
+            var builder = new ContainerBuilder();            
+            builder.RegisterType<GameMachineRepository>().As<IGameMachineRepository>();  
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
